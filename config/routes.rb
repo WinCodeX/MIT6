d9c92198a8e9bddb5b4b0466c038d52708ce9bf6
WinCodeX <glenwinterg970@gmail.com>
2023-03-06 15:43:39 +0300

    Test

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root "home#index"
+  get "about", to: "home#about"
+  get "contact", to: "home#contact"
+  get "search", to: "course#search"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
